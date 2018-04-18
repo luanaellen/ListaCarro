1854a0b5f1f332fbae73962dd1fca04f1cba63b7
luanaellen <luana_ellen13@hotmail.com>
2018-04-18 20:08:44 -0300

    Primeiro commit carros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public carros;
   constructor(public navCtrl: NavController) {
 
+    this.carros=[
+      {nome:"Uno", preco:32000},
+      {nome:"Fox", preco:75000},
+      {nome:"HB20", preco:95000},
+      {nome:"Cobalt", preco:70000},
+      {nome:"Discovery", preco:10000},
+    ];
   }
 
 }
